322bea4f97b64825da43cda7579d6caf1134e578
Jochem Gravendeel <jochemgravendeel@gmail.com>
2020-09-01 10:23:22 +0200

    opdr 1 af

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,21 +1,23 @@
 <?php
 $medewerkers = [
     [
-      'voornaam' => 'Willem',
-      'achternaam' => 'van Oranje',
-      'functie'  => 'koning'
+      'voornaam' => ' Willem ',
+      'achternaam' => ' van Oranje ',
+      'functie'  => ' koning '
     ],
     [
-      'voornaam' => 'Donald',
-      'achternaam' => 'Trump',
-      'functie'  => 'president'
+      'voornaam' => ' Donald ',
+      'achternaam' => ' Trump ',
+      'functie'  => ' president '
     ]
   ];
-  
-  /* De medewerkers-array heeft twee arrays. Een array op index 0 en een array op index 1
-   *  Om een enkele waarde op het scherm te tonen doe je bijvoorbeeld dit.
-   *
-   *   <--medewerker-> */
-  echo $medewerkers[0]['voornaam'] // Willem
-  echo $medewerkers[1]['voornaam', 'achternaam', 'functie'] // Donald
+
+  echo $medewerkers [0]['voornaam'];
+  echo $medewerkers [0]['achternaam'];
+  echo ' is een '.$medewerkers [0]['functie'] . '<br>';
+
+  echo $medewerkers [1]['voornaam'];
+  echo $medewerkers [1]['achternaam'];
+  echo ' is een '.$medewerkers [1]['functie'] . '<br>';
+
   
