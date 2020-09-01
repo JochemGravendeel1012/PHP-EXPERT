37f8a3f2f443802dfe81528e9109b5b721075e5e
Jochem Gravendeel <jochemgravendeel@gmail.com>
2020-09-01 10:06:41 +0200

    1

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,21 @@
-
+<?php
+$medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ]
+  ];
+  
+  /* De medewerkers-array heeft twee arrays. Een array op index 0 en een array op index 1
+   *  Om een enkele waarde op het scherm te tonen doe je bijvoorbeeld dit.
+   *
+   *   <--medewerker-> */
+  echo $medewerkers[0]['voornaam'] // Willem
+  echo $medewerkers[1]['voornaam', 'achternaam', 'functie'] // Donald
+  
